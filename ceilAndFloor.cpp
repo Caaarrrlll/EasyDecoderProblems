bc88e8b6ad9d3f4e8128237b350ebbb7334084e9
Carl <venter109@gmail.com>
2018-10-07 15:23:57 +0200

    firstEasyProblem

---
+++ ceilAndFloor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int ceil(double inNo){
+	int inn = (int)inNo;
+	if(inNo == (float)inn){
+		return inn;
+	}
+	return (int)inNo + 1;
+}
+
+int floor(double inNo){
+	int inn = (int)inNo;
+	if(inNo == (float)inn){
+		return inn;
+	}
+	return (int)inNo - 1;
+}
+
+int main(){
+	double inNum;
+	cin >> inNum;
+	cout << ceil(inNum) << " " << floor(inNum) << endl;
+}
